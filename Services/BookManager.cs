a40dc5693df6a69292b0ee1cb5106a5c463ccca6
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-07-21 00:18:41 +0300

    Service add

---
+++ Services/BookManager.cs
@@ -0,0 +1,64 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal class BookManager : IBookService
+    {
+        private readonly IRepositoryManager manager;
+
+        public BookManager(IRepositoryManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public Book CreateOneBook(Book book)
+        {
+            if(book is null)
+                throw new ArgumentNullException(nameof(book));
+            manager.Book.CreateOneBook(book);
+            manager.Save();
+            return book;
+        }
+
+        public void DeleteOneBook(int id, bool trackChanges)
+        {
+            var entity = manager.Book.GetOnBookById(id, trackChanges);
+            if (entity is null)
+                throw new Exception($"Book with {id} could not fount");
+
+            manager.Book.DeleteOneBook(entity);
+            manager.Save();
+        }
+
+        public IEnumerable<Book> GetAllBooks(bool trackChanges)
+        {
+           return manager.Book.GetAllBooks(trackChanges);
+        }
+
+        public Book GetOneBook(int id, bool trackChanges)
+        {
+            return manager.Book.GetOnBookById(id,trackChanges);
+        }
+
+        public Book UpdateOneBook(int id, Book book, bool trackChanges)
+        {
+            var entity = manager.Book.GetOnBookById(id,trackChanges );
+            if (entity is null)
+                throw new Exception($"Book with {id} could not fount");
+            if (book is null)
+                throw new ArgumentException(nameof(book));
+            entity.Title=book.Title;
+            entity.Price = book.Price;
+            
+            manager.Book.UpdateOneBook(entity);
+            return entity;
+        }
+    }
+}
