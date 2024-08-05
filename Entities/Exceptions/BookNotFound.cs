e7c2c1d6ceb9a5062995b472a72ba65ab0130f54
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-08-05 21:38:30 +0300

    custom exception

---
+++ Entities/Exceptions/BookNotFound.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class BookNotFound : NotFound
+    {
+        public BookNotFound(int id) : base($"The Book with id:{id} could not found.")
+        {
+        }
+    }
+}
