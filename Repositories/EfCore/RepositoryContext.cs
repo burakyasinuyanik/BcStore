fa44fee22330a82879de00ddbeb97da7ffe353aa
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-09-01 18:19:00 +0300

    category p

--- Repositories/EfCore/RepositoryContext.cs
+++ Repositories/EfCore/RepositoryContext.cs
@@ -24,6 +24,7 @@ namespace Repositories.EfCore
 
         }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ WebApi/Migrations/20240901150927_newCategoryClassAdd.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class newCategoryClassAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7bdac596-d189-464a-ba29-6a13cf0c4a27");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d2647aa9-ef4b-4da4-809b-a359a5df316b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f6dc750c-a78e-4950-955d-f91fe22d9378");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1cea3471-f1af-409d-9007-f4ebe87f3844", null, "Editor", "EDITOR" },
+                    { "a238be31-ce80-42c0-8cfe-89a930725f79", null, "User", "USER" },
+                    { "c2fed57d-1fcc-4aa5-bae7-0c879073433e", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Computer Science" },
+                    { 2, "Network" },
+                    { 3, "DataBase Magement Systems" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1cea3471-f1af-409d-9007-f4ebe87f3844");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a238be31-ce80-42c0-8cfe-89a930725f79");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c2fed57d-1fcc-4aa5-bae7-0c879073433e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7bdac596-d189-464a-ba29-6a13cf0c4a27", null, "User", "USER" },
+                    { "d2647aa9-ef4b-4da4-809b-a359a5df316b", null, "Editor", "EDITOR" },
+                    { "f6dc750c-a78e-4950-955d-f91fe22d9378", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
